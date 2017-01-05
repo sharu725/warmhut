e861d56d16fc327e19088b7f57926aed17b617a8
dan <onewesh@gmail.com>
2017-01-05 19:33:26 +0300

    remove font awesome

--- assets/index.js
+++ assets/index.js
@@ -6,9 +6,9 @@
           $(this).siblings('.nav-menu').toggleClass('menu-open');
         });
         $('.bot').on('click hover', function(){
-          bot = $(this).children('.fa-2x');
+          bot = $(this).children('.icon');
           $('.widget').toggleClass('open');
-          bot.toggleClass('fa-comments').toggleClass('fa-times-circle');
+          bot.toggleClass('icon-chat').toggleClass('icon-cancel');
         });
         $('.fa-close').on('click', function() {
           $('.widget').removeClass('open');
@@ -34,87 +34,7 @@
             $('.overlay').append('banner');
           }
         });
-    });
-
-    year = new Date();
+           year = new Date();
     $('.thisYear').append(year.getFullYear());
+    });
 })();
-
-var Bubbles = (function () {
-    function Bubbles() {
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
-        this.particles = [];
-      	this.counter = 0;
-    } 
-    Bubbles.prototype.init = function () {
-        this.createElement();
-        this.loop();
-    };
-    Bubbles.prototype.createElement = function () {
-        var scale = 1;
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.canvas.style.background = 'transparent';
-        this.canvas.style.width = '100%';
-        this.ctx.transform(scale, 0, 0, -scale, this.canvas.width / 2, this.canvas.height / 2);
-        var container = document.getElementById('bubbles');
-        if(container) {
-            container.appendChild(this.canvas);
-            for(var i = 0; i < 400; i++) {
-                this.createParticle();
-            }
-        }
-    };
-    Bubbles.prototype.createParticle = function () {
-        this.particles.push({
-            color: Math.random() > 0.5 ? "rgba(255, 255, 255, 0.9)" : "rgba(0, 255,0, 0.4)",
-            radius: Math.random() * 16,
-            x: (Math.random() - 0.5) * 25,
-            y: - this.canvas.height / 2,
-            xVel: (Math.random() - 0.5) * 2,
-            yVel: (Math.random() + 0.5)
-        });
-    };
-    Bubbles.prototype.disappearParticle = function (p) {
-        if (p.radius < 0.5) {
-            this.resetParticle(p);
-        }
-        p.radius *= 0.994;
-    };
-    Bubbles.prototype.resetParticle = function (p) {
-        p.radius = Math.random() * 16;
-        p.x = (Math.random() - 0.5) * 25;
-        p.y = - this.canvas.height / 2;
-    };
-    Bubbles.prototype.moveParticle = function (p) {
-        p.x += p.xVel;
-        p.y += p.yVel;
-    };
-    Bubbles.prototype.draw = function (p) {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = p.color;
-        this.ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
-      	this.ctx.fill();
-    };
-    Bubbles.prototype.loop = function () {
-        var _this = this;
-      	if (this.counter < this.particles.length) {
-      			this.counter ++;
-        }
-        this.ctx.clearRect(-this.canvas.width / 2, -this.canvas.height / 2, this.canvas.width, this.canvas.height);
-        for (var i = 0; i < this.counter; i++) {
-            this.moveParticle(this.particles[i]);
-            this.disappearParticle(this.particles[i]);
-            this.draw(this.particles[i]);
-        }
-        requestAnimationFrame(function () { return _this.loop(); });
-    };
-    return Bubbles;
-})();
-window.onload = function () {
-		var particles = new Bubbles();
-		particles.init();
-};
-
-
