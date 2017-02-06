7823e5c9369ac5c20ec86bf6ba6b6e585fa457e9
peter patel <peter patel>
2017-02-06 19:30:29 +0300

    add custom js

--- custom.js
+++ custom.js
@@ -1,1 +1,22 @@
-//avoid jquery
+/*avoid jquery*/
+//addClass
+//removeClass
+//toggleClass
+//serialize
+//find
+//ajax
+
+
+function addClass(className){
+  document.getElementsByClassName(selector).classList.add(className);
+}
+function removeClass(className){
+  document.getElementsByClassName(selector).classList.remove(className);
+}
+function toggleClass(className){
+  document.getElementsByClassName(selector).classList.toggle(className);
+}
+function find(itemName){
+  el.querySelectorAll(selector);
+}
+
--- index.md
+++ index.md
@@ -1,6 +1,6 @@
 ---
 layout: page
-title: Warmhut | Build your brand with us
+title: Build your brand with us
 description: 'We are web and marketing experts. We believe in your creativity and resolve to make a difference. We have developed a set of solutions that will help you transform your ideas to profitability.'
 keywords: 'Warmhut, business, start website, brand, social media, enable enterpreneur, promote business, awareness, warmhutgroup, technology'
 ---
